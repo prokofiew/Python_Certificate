f6259257baa90d36281606510b87d956c61a1edd
prokofiew <filipmaciborski@gmail.com>
2024-03-18 23:35:01 +0100

    practicing loops

--- 14_for_and_while.py
+++ 14_for_and_while.py
@@ -7,5 +7,19 @@
 #     # do_something()
 #     pass
 
-for i in range(10):
-    print("The value of i is:", i)
+# for i in range(10):
+#     print("The value of i is:", i)
+#
+for i in range(2, 8):
+   print("The value of i is currently", i)
+
+for i in range(2, 8, 3): # step 3
+    print("The value of i is currently", i)
+
+
+# this will generate empty
+for i in range(2, 1):
+   print("The value of i is currently", i)
+
+for i in range(2, 1):
+   print("The value of i is currently", i)
